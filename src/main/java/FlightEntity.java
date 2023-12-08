431f6e19711f786a2fbfe338c31679ba17802396
Voronovich2021 <vovavoronovic72@gmail.com>
2023-12-08 13:52:29 +0300

    itog

---
+++ src/main/java/FlightEntity.java
@@ -0,0 +1,17 @@
+import lombok.Data;
+
+@Data
+public class FlightEntity {
+    private String number;
+    private RouteEntity routes;
+    private TimeEntity timer;
+    private String flight;
+
+    @Override
+    public String toString() {
+        return "Рейс{" +
+                "Номер = '" + number +'\'' + routes + '\''+ "Дата и время:{ " +  timer +
+                ", Рейс = '" + flight + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/RouteEntity.java
@@ -0,0 +1,16 @@
+import lombok.Data;
+
+@Data
+public class RouteEntity {
+private String route;
+private String stop;
+private String place;
+
+    @Override
+    public String toString() {
+        return "Маршрут = '" + route + '\'' +
+                ", Остановки по пути = '" + stop + '\'' +
+                ", Место = '" + place + '\'' +
+                '}';
+    }
+}
